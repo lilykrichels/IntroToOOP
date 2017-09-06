3fb34c5c36d830c25c5287804d09de9b561975e4
Lily Krichels <lily.krichels@gmail.com>
2017-09-06 19:35:00 -0400

    added Cat class

---
+++ IntroToOOP/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Cat
+    {
+        //these are FIELDS. they are private so they cannot be changed 
+        private string name;
+        private string furColor;
+        private int furLength;
+        private int age;
+        private bool isMale;
+        private bool isHungry;
+
+        //these are PROPERTIES they are public and allow the user to access the private fields
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string FurColor
+        {
+            get { return this.furColor;}
+            set { this.furColor = value; }
+        
+        }
+
+        public int Age
+        {
+            get { return this.age; }  //not letting the user change the age, starts at zero
+        }
+        
+
+        public Cat()
+        {
+
+        }
+
+        //this is constructor which requires 
+        public Cat(string name, string furColor, int furLength, int age, bool isMale, bool isHungry)
+        {
+            this.name = name;
+            this.furColor = furColor;
+            this.furLength = furLength;
+            this.age = age;
+            this.isMale = isMale;
+        }
+
+    }
+}
--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -10,6 +10,20 @@ namespace IntroToOOP
     {
         static void Main(string[] args)
         {
+            //creating an instance of a Cat
+            //must input the information for the field
+            // the blank one is the default
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat("Auggie", "grey", 4, 10, true, true);
+
+            cat1.Name = "gato";
+            cat1.FurColor = "black";
+
+            //prints the Cat's names to the console.
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+            Console.WriteLine((cat1.FurColor + cat1.Name));
+            
         }
     }
 }
